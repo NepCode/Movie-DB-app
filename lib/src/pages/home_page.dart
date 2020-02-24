ffd3cf7ab3f1c3f71c354e11aaa9b4cd9013507a
Antonio Aguila <aguilantonio16@gmail.com>
2020-02-23 21:38:24 -0600

    swiper added

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:movies_app/src/widgets/card_swiper_widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Movies in theaters'),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[_swiperCards()],
+        ),
+      ),
+      /* body: SafeArea(child:
+      Text('Hello World')
+      ),*/
+    );
+  }
+
+  Widget _swiperCards() {
+    return CardSwiper(movies: [1,2,3,4,5]);
+  }
+}
